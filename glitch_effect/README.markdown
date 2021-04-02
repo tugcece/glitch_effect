165863e0b09905ea802d1f5d1adc29889f32e53b
tugcece <79104524+tugcece@users.noreply.github.com>
2021-04-02 21:29:53 +0300

    Add files via upload

---
+++ glitch_effect/README.markdown
@@ -0,0 +1,5 @@
+# GlitchEffect
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/tugcece/pen/ExZWqPb](https://codepen.io/tugcece/pen/ExZWqPb).
+
+
---
+++ glitch_effect/dist/script.js
@@ -0,0 +1,8 @@
+const color = document.querySelector(".glitch");
+var x = document.getElementsByTagName("BODY")[0];
+color.addEventListener("click",change);
+
+function change(){ 
+ color.style.color="red"; 
+ x.style.transitionDuration="15sn";  x.style.backgroundImage="url('https://i.hizliresim.com/K6Sux5.png')";
+}
